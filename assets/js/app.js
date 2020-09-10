23e4aaa773199a359c1d16c706782dde3c3698cc
Maximiliano Ponce Marquez <maxponce.marquez@gmail.com>
2020-09-09 23:55:25 -0700

    Refactoring page: home, about, skills, education

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,91 +1,40 @@
-/* ===== MENU SHOW ===== */
-const showMenu = (toggleId, navId) => {
-    const toggle = document.getElementById(toggleId),
-    nav = document.getElementById(navId)
+// <== Menu ==>
+const navMenu = document.getElementById('nav-menu');
+const toggleMenu = document.getElementById('nav-toggle');
+const closeMenu = document.getElementById('nav-close');
 
-    if(toggle && nav) {
-        toggle.addEventListener('click', () => {
-            nav.classList.toggle('show')
-        })
-    }
+// <= Show Menu =>
+toggleMenu.addEventListener('click',()=>{
+    navMenu.classList.toggle('show');
+});
+
+// <= Close Menu =>
+closeMenu.addEventListener('click',()=>{
+    navMenu.classList.remove('show');
+});
+
+// <= Remove Menu =>
+const navLink = document.querySelectorAll('.nav_link');
+
+function linkAction(){
+    navMenu.classList.remove('show');
 }
+navLink.forEach(n => n.addEventListener('click', linkAction));
 
-showMenu('nav-toggle', 'nav-menu')
-
-// ===== ACTIVE AND REMOVE MENU =====
-const navLink = document.querySelectorAll('.nav_link')
-
-function linkAction() {
-    //  Active Link
-    navLink.forEach(n => n.classList.remove('active'))
-    this.classList.add('active')
-
-    //  Remove Menu once you choose an option
-    const navMenu = document.getElementById('nav-menu')
-    navMenu.classList.remove('show')
-}
-
-navLink.forEach(n => n.addEventListener('click', linkAction))
-
-// ===== CHANGE NAVIGATION STYLE ON SCROLL =====
-
-// window.addEventListener('scroll', event => {
-//     let header = document.querySelector('.l-header');
-//     (window.scrollY >= 20) ? header.classList.add('scroll') : header.classList.remove('scroll');
-// });
-
-const sections = document.querySelectorAll('section[id]')
-window.addEventListener('scroll', scrollActive)
+// <=== Active Link ===>
+const sections = document.querySelectorAll('section[id]');
 
 function scrollActive(){
-    const scrollY = window.pageYOffset
-
+    const scrollY = window.pageYOffset;
     sections.forEach(current => {
         const sectionHeight = current.offsetHeight;
-        const sectionTop = current.offsetTop - 10;
-        sectionId = current.getAttribute('id')
-        // console.log(sectionId)
+        const sectionTop = current.offsetTop - 50;
+        sectionId = current.getAttribute('id');
         if(scrollY > sectionTop && scrollY <= sectionTop + sectionHeight){
-            document.querySelector('.nav_menu a[href*=' + sectionId + ']').classList.add('active')
-        }else{
-            document.querySelector('.nav_menu a[href*=' + sectionId + ']').classList.remove('active')
+            document.querySelector('.nav_menu a[href*='+ sectionId + ']').classList.add('active');
+        } else {
+            document.querySelector('.nav_menu a[href*='+ sectionId + ']').classList.remove('active');
         }
-    })
+    });
 }
-
-/* ===== SCROLL REVEAL ANIMATIONS ===== */
-const sr = ScrollReveal({
-    origin:'top',
-    distance: '80px',
-    duration: 2000,
-    reset: true
-})
-
-// => HOME
-sr.reveal('.home_title', {origin:'top'})
-sr.reveal('.home_img', {origin: 'bottom', delay: 100})
-sr.reveal('.home_social', {origin: 'bottom', delay: 120, interval: 200})
-sr.reveal('.button', {origin: 'left', delay: 140})
-
-// => ABOUT
-sr.reveal('.about_img', {})
-sr.reveal('.about_subtitle', {delay: 100})
-sr.reveal('.about_text', {origin:'left', delay: 120})
-
-// => SKILLS
-sr.reveal('.skills_subtitle', {})
-sr.reveal('.skills_container', {delay: 100})
-sr.reveal('.skills_img', {origin:'bottom', delay: 120})
-sr.reveal('.skills_text', {origin:'left', delay: 140})
-
-// => PROJECTS
-sr.reveal('.card-1', {origin: 'up', delay: 80})
-sr.reveal('.card-2', {origin: 'bottom', delay: 100})
-sr.reveal('.card-3', {origin: 'up', delay: 120})
-sr.reveal('.card-4', {origin: 'bottom', delay: 140})
-sr.reveal('.btn', {origin: 'left', delay: 160})
-sr.reveal('.btn-live', {origin: 'right', delay: 160})
-
-// => CONTACT
-sr.reveal('.contact_container', {origin: 'bottom'})
-sr.reveal('.button', {origin: 'left', delay: 100})
+window.addEventListener('scroll', scrollActive);
